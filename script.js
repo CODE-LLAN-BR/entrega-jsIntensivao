65275ee381ea24815dd2ab4fcb9f52e40716adeb
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-07-25 22:08:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+/*Você pode apresentar o resultado tanto no **console** quanto em um **alerta** no navegador.
+
+1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+*/
+    //resolução:
+
+    // alert('Hello World!')
+
+/*
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+*/
+    //resolução:
+    
+    // let num1 = 10;
+    // let num2 = 30;
+    // alert(num1+num2)
+/*
+
+3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+*/
+    //resolução:
+    // let variable = 5;
+
+
+    // if(typeof variable === 'number'){
+    //     console.log('É um número')
+    // }else{
+    //     console.log('Não é um número')
+    // }
+
+    
+    
+    
+/*   
+💡 Para saber o tipo de dado você pode usar o operador `typeof`
+    
+4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+*/
+    //Resolução:
+    // let variable = 1;
+    // if(typeof variable === 'string'){
+    //     console.log('É uma String')
+    // }else{
+    //     console.log('Não é uma String')
+    // }
+/*
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+*/
+    //Resolução:
+    // let variable = 'true';
+    // if(typeof variable === 'boolean'){
+    //     console.log('É um booleano')
+    // }else{
+    //     console.log('Não é um booleano')
+    // }
+/*
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+*/
+    // //Resolução:
+    // let num1 = 25;
+    // let num2 = 32;
+    // console.log(num1 + num2)
+/*
+
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+*/
+    //Resolução:
+    // let num1 = 23;
+    // let num2 = 35;
+    // console.log(num1 * num2)
+/*
+
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+*/
+        // //Resolução:
+        // let num1 = 100;
+        // let num2 = 20;
+        // console.log(num1 / num2)
+/*
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+*/
+        // //Resolução:
+        // let num = 26;
+        // if(num % 2 == 0 ){
+        //     console.log('É um número Par')
+        // }else{
+        //     console.log('Não é um número Par')
+        // }
+
+
+/*
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+*/
+        // //Resolução:
+        // let num = 6;
+        // if(num % 2 == 0 ){
+        //     console.log('Não é um número Impar')
+        // }else{
+        //     console.log('É um número impar')
+        // }
